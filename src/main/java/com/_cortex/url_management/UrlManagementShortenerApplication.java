1b360df94169f988b138fba93db58dfe79fc10ff
Ishu Mehta <mehta.ishu14@gmail.com>
2025-11-20 14:24:02 +0530

    first commit

---
+++ src/main/java/com/_cortex/url_management/UrlManagementShortenerApplication.java
@@ -0,0 +1,13 @@
+package com._cortex.url_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlManagementShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlManagementShortenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_cortex/url_management/UrlManagementShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com._cortex.url_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlManagementShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
